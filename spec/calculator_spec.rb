80ea9f3c6e4557e5cbc9f294bc19e049490a03ad
Student <student@Makerss-MacBook-Pro.local>
2019-10-17 12:18:54 +0100

    Calculator Challenge

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,17 @@
+require 'calculator'
+
+describe Calculator do
+
+  it { is_expected.to respond_to :add }
+
+  it "does the add method add two arguments" do
+    expect(subject.add(1,2)).to eq (3)
+  end
+
+  it { is_expected.to respond_to :subtract }
+
+  it "does the subtract method subtract two arguments" do
+    expect(subject.subtract(3,2)).to eq (1)
+  end
+
+end
